c6fe5575676e388371355c3ce40a3fe0e6aeb52c
Milena Michalska <milena.mm@wp.pl>
2020-05-17 09:44:48 +0200

    Lab2 sourcefile added

---
+++ LAB02/TD_LAB02.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 39362
+b = 6
+c = 3
+
+A = 1.0
+f = b
+fi = c * np.pi
+
+def s(A,f,fi,t):
+    return A * np.sin(2 * np.pi * f * t + fi)
+
+def zad1():
+    #sygnal sprobkowany
+    t = np.linspace(0,2,200)
+    plt.plot(t, s(A, f, t, fi))
+    plt.show()
+
+def zad2():
+    q=16
+    t = np.linspace(0,1,200)
+
+    plt.step(t, np.round((s(A,f,t,fi)+A)*(q**2/2)))
+    plt.plot(t, np.round((s(A,f,t,fi)+A)*(q**2/2)))
+    plt.show()
+    
+def zad3():
+    q=8
+    t = np.linspace(0,1,100)
+    plt.step(t, np.round((s(A,f,t,fi)+A)*(q**2/2)))
+    plt.plot(t, np.round((s(A,f,t,fi)+A)*(q**2/2)))
+    plt.show()
+
+zad1()
+zad2()
+zad3()
